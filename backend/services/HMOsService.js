74b95074cf96757d20a7861d0f1b485b6a977524
MiriTenenboim <MiryamT@darkeychana.com>
2024-06-18 20:27:51 +0300

    HMO

--- backend/services/HMOsService.js
+++ backend/services/HMOsService.js
@@ -3,3 +3,34 @@ const HMOModel = require('../models/HMOModel');
 exports.findHMOById = (id) => {
     return HMOModel.findByPk(id);
 }
+
+exports.findAllHMOs = () => {
+    return PatientModel.findAll();
+}
+
+
+exports.updateHMO = (id, HMOData) => {
+    return HMOModel.update(HMOData, {
+        where: { id: id }
+    });
+}
+
+exports.deleteHMO = (id) => {
+    return HMOModel.destroy({
+        where: { id: id }
+    });
+}
+
+exports.createHMO = async (name) => {
+    try {
+        const data = {
+            Name: name
+        };
+
+        const HMOEnter = await HMOModel.create(data);
+        
+        return HMOEnter;
+    } catch (error) {
+        throw new Error(error.message);
+    }
+};
