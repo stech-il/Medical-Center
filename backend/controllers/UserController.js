4c6eb28c323a61676bd2b337aa23db91bed4004e
MiriTenenboim <MiryamT@darkeychana.com>
2024-06-23 23:01:57 +0300

    login

--- backend/controllers/UserController.js
+++ backend/controllers/UserController.js
@@ -1,8 +1,32 @@
+const UsersModel = require('../models/UserModel');
 const UsersService = require('../services/UsersService');
+
+const bcrypt = require('bcryptjs');
 
 exports.findUserById = async (req, res) => {
     try {
         const user = await UsersService.findUserById(req.params.id);
+        if (user) {
+            return res.json({
+                data: user,
+                message: 'Success.'
+            });
+        } else {
+            return res.status(404).json({
+                message: 'User not found.'
+            });
+        }
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Internal Server Error',
+            error: error.message
+        });
+    }
+}
+
+exports.findUserByEmailAddress = async (req, res) => {
+    try {
+        const user = await UsersService.findUserByEmailAddress(req.params.Email);
         if (user) {
             return res.json({
                 data: user,
@@ -38,11 +62,56 @@ exports.findAllUsers = async (req, res) => {
 
 exports.createUser = async (req, res) => {
     try {
-        const newUser = await UsersService.createUser(req.body);
+        bcrypt.genSalt(10, (err, salt) => {
+            if (!err) {
+                bcrypt.hash(req.body.Password, salt, function(err, hashPassword) {
+                    if (!err) {
+                        console.log("hashPassword ", hashPassword);
+                        req.body.Password = hashPassword;
+                        UsersService.createUser(req.body);
+                    }
+                });
+            }
+        });
+        
         return res.status(201).json({
-            data: newUser,
+            data: req,
             message: 'User created successfully.'
         });
+    } catch (error) {
+        return res.status(500).json({
+            message: 'Internal Server Error',
+            error: error.message
+        });
+    }
+}
+
+exports.userLogin = async (req, res) => {
+    // try {
+    //     const user = await UsersService.login(req.body.Email, req.body.Password);
+    //     return res.json({
+    //         data: user,
+    //         message: 'Success.'
+    //     });
+    // } catch (error) {
+    //     return res.status(500).json({
+    //         message: 'Internal Server Error',
+    //         error: error.message
+    //     });
+    // }
+    try {
+        const { email, password } = req.body;
+        const loggedIn = await UsersService.userLogin(email, password);
+        console.log(loggedIn);
+        if (loggedIn) {
+            return res.json({
+                message: 'Login successful'
+            });
+        } else {
+            return res.status(401).json({
+                message: 'Login failed. Invalid credentials.'
+            });
+        }
     } catch (error) {
         return res.status(500).json({
             message: 'Internal Server Error',
--- backend/services/UsersService.js
+++ backend/services/UsersService.js
@@ -1,5 +1,7 @@
 const UsersModel = require('../models/UserModel');
 
+const bcrypt = require('bcryptjs');
+
 exports.findUserById = (id) => {
     return UsersModel.findByPk(id);
 }
@@ -10,10 +12,42 @@ exports.findAllUsers = () => {
 
 exports.createUser = async (userData) => {
     try {
+        console.log(userData);
         const newUser = await UsersModel.create(userData);
         return newUser;
     } catch (error) {
         throw new Error(error.message);
+    }
+}
+
+exports.findUserByEmailAddress = (emailAddress) => {
+    return UsersModel.findOne({ Email: emailAddress });
+}
+
+exports.userLogin = async (emailAddress, password) => {
+    // const user = this.findUserByEmailAddress(emailAddress);
+    // bcrypt.compare(password, user.Password, (err, res) => {
+    //     if (res === true) {
+    //         return true;
+    //     }
+    //     else {
+    //         return false;
+    //     }
+    // })
+    try {
+        const user = await this.findUserByEmailAddress(emailAddress);
+        if (!user) {
+            return false; // User not found
+        }
+        console.log("true");
+        const passwordMatch = await bcrypt.compare(password, user.Password);
+        console.log(passwordMatch);
+        if (passwordMatch === true) {
+            return true;
+        }
+        return false;
+    } catch (error) {
+        throw error; // Handle error appropriately
     }
 }
 
