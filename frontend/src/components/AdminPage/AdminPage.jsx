acdeacbb7b4126889b1dafa44a2d7f39e0792d6c
MiriTenenboim <MiryamT@darkeychana.com>
2024-08-14 20:07:08 +0300

    update login

--- frontend/src/components/AdminPage/AdminPage.jsx
+++ frontend/src/components/AdminPage/AdminPage.jsx
@@ -1,34 +1,34 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import SidebarMenu from '../sidebar/sidebar'
 import './AdminPage.css'
-import { Navigate, useLocation } from 'react-router-dom';
+import { useLocation, useNavigate } from 'react-router-dom';
 const AdminPage = () => {
 
     const location = useLocation();
     const role = location.state;
 
+    const navigate = useNavigate();
+
+    useEffect(() => {
+        if (!sessionStorage.getItem('email')) {
+            navigate('/userLogin');
+        }
+    }, []);
+
     return (
         <>
-            {sessionStorage.getItem('email') && (<div className='AdminPageContainer' style={{ direction: "rtl" }}>
-                <SidebarMenu role={role} />
-                <div>ניהול מערכת</div>
-                <div>
-                    <div>עדכון אחרון</div>
-                    <div>15-02-2024 14:32</div>
-                </div>
-            </div>)}
             {sessionStorage.getItem('email') ? (
-                        <div className='AdminPageContainer' style={{ direction: "rtl" }}>
-                        <SidebarMenu role={role} />
-                        <div>ניהול מערכת</div>
-                        <div>
-                            <div>עדכון אחרון</div>
-                            <div>15-02-2024 14:32</div>
-                        </div>
+                <div className='AdminPageContainer' style={{ direction: "rtl" }}>
+                    <SidebarMenu role={role} />
+                    <div>ניהול מערכת</div>
+                    <div>
+                        <div>עדכון אחרון</div>
+                        <div>15-02-2024 14:32</div>
                     </div>
-                    ) : (
-                        Navigate('/userLogin')
-                    )}
+                </div>
+            ) : (
+                <div>Redirecting...</div>
+            )}
         </>
 
     );
