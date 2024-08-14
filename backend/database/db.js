4f128654afaa931f07f03324ca0215708595dc7e
tehila1111 <116339153+tehila12@users.noreply.github.com>
2024-08-14 22:56:52 +0300

    more updates

--- backend/database/db.js
+++ backend/database/db.js
@@ -2,8 +2,8 @@ const { Sequelize } = require('sequelize');
 
 const db = new Sequelize(process.env.DB_NAME,  process.env.DB_USER, process.env.DB_PASSWORD, 
 {
-    host: "localhost",
-    port: "3306",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
     dialect: "mysql",
     define: {
         timestamps: false
