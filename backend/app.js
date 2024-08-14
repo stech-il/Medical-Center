b5b86388d148fe3e9be263347478aab35c7832af
Shoshi Sternberger <shoshis@darkeychana.com>
2024-08-14 17:39:01 +0300

    env file

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require('cors');
+require('dotenv').config();
 const db = require('./database/db.js');
 const createSocketServer = require('./services/SocketServer.js')
 
